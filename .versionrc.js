c6c414d22546749506229c554123f9429ed074cb
likan <LIiErkang>
2022-08-25 18:16:32 +0800

    doc: 添加versionrc文件

---
+++ .versionrc.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+/**
+ * @Author likan
+ * @Date 2022/8/25 14:17:39
+ * @FilePath E:\TestSpace\extension\likan\.versionrc.js
+ */
+
+/** @type {import('standard-version').Options} */
+const VersionConfig = {
+  commitAll: true,
+  scripts: {},
+};
+
+module.exports = VersionConfig;
