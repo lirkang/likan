89d4f03571664027d9c4f554dfc42ddfdbb9980e
李尔康 <li0623for@gmail.com>
2022-10-24 23:50:55 +0800

    perf: 优化tag wrap

--- src/commands/tags-wrap.ts
+++ src/commands/tags-wrap.ts
@@ -8,72 +8,111 @@ import { times } from 'lodash-es';
 
 import Editor from '@/classes/Editor';
 
-export default async function tagsWrap ({ document, selections, selection, options }: vscode.TextEditor) {
-  if (selections.length > 1) return;
+interface TagWrapHandler {
+  (textEditor: vscode.TextEditor, editor: Editor, tabSize: string): [
+    startPosition: vscode.Position,
+    endPosition: vscode.Position
+  ];
+}
 
-  const { start, isEmpty, isSingleLine, end } = selection;
-  const { lineAt, uri } = document;
-  const editor = new Editor(uri);
-  const { range, text } = lineAt(start.line);
-  const tabSize = options.insertSpaces ? ' '.repeat(<number>options.tabSize) : '\t';
+const isEmptyAndNotOnLastCharacterAndEmpty: TagWrapHandler = ({ selection }, editor) => {
+  const { start } = selection;
+
+  editor.insert(start, `<${Configuration.tag}>`);
+  editor.insert(start, `</${Configuration.tag}>`);
+
+  return [ start.translate(0, 1 + Configuration.tag.length), start.translate(0, 1 + Configuration.tag.length * 2 + 3) ];
+};
+
+const isEmptyAndOnLastCharacter: TagWrapHandler = ({ document, selection }, editor, tabSize) => {
+  const { start, end } = selection;
+  const { text } = document.lineAt(start.line);
   const match = text.match(/^(?<space>^\s*?)\S/);
   const space = match?.groups?.space ?? '';
-  const frontText = text.slice(0, start.character);
-  const behindText = text.slice(start.character, range.end.character);
-  const startTranslate = (line = 0, character = -start.character) => start.translate(line, character);
 
-  let startTagPosition: vscode.Position;
-  let endTagPosition: vscode.Position;
+  editor.insert(start.translate(0, -start.character), `${space}<${Configuration.tag}>\n${tabSize}`);
+  editor.insert(end, `\n${space}</${Configuration.tag}>`);
 
-  if (isEmpty)
-    if (start.character === range.end.character && lineAt(start.line).text.trim().length > 0) {
-      editor.insert(start.translate(0, -start.character), `${space}<${Configuration.tag}>\n${tabSize}`);
-      editor.insert(end, `\n${space}</${Configuration.tag}>`);
+  return [
+    new vscode.Position(start.line, space.length + 1 + Configuration.tag.length),
+    new vscode.Position(start.line + 2, space.length + 2 + Configuration.tag.length),
+  ];
+};
 
-      startTagPosition = new vscode.Position(start.line, space.length + 1 + Configuration.tag.length);
-      endTagPosition = new vscode.Position(start.line + 2, space.length + 2 + Configuration.tag.length);
-    } else {
-      editor.insert(start, `<${Configuration.tag}>`);
-      editor.insert(start, `</${Configuration.tag}>`);
+const isSingleLineAndNotEmpty: TagWrapHandler = ({ selection }, editor) => {
+  const { start, end } = selection;
 
-      startTagPosition = startTranslate(0, 1 + Configuration.tag.length);
-      endTagPosition = startTranslate(0, 1 + Configuration.tag.length * 2 + 3);
-    }
-  else if (isSingleLine) {
-    editor.insert(start, `<${Configuration.tag}>`);
-    editor.insert(end, `</${Configuration.tag}>`);
+  editor.insert(start, `<${Configuration.tag}>`);
+  editor.insert(end, `</${Configuration.tag}>`);
 
-    startTagPosition = new vscode.Position(start.line, start.character + 1 + Configuration.tag.length);
-    endTagPosition = new vscode.Position(end.line, end.character + 2 + Configuration.tag.length * 2 + 2);
-  } else {
-    const startSpace = behindText.slice(0, behindText.length - behindText.trimStart().length);
-    const nextLineStartSpace = frontText.slice(0, behindText.length - behindText.trimStart().length);
+  return [
+    new vscode.Position(start.line, start.character + 1 + Configuration.tag.length),
+    new vscode.Position(end.line, end.character + 2 + Configuration.tag.length * 2 + 2),
+  ];
+};
 
+const otherwiseHandler: TagWrapHandler = ({ selection, document }, editor, tabSize) => {
+  const { start, end } = selection;
+  const { text, range } = document.lineAt(start.line);
+  const frontText = text.slice(0, Math.max(0, start.character));
+  const behindText = text.slice(start.character);
+
+  if (/\S/.test(frontText)) {
     editor.insert(start, `\n<${Configuration.tag}>\n`);
-    editor.insert(end, `\n</${Configuration.tag}>\n`);
+    editor.insert(end, `\n</${Configuration.tag}>`);
 
-    times(Math.abs(end.line - start.line), index => {
-      const line = start.line + index + 1;
-      const { text } = lineAt(line);
+    times(Math.abs(start.line - end.line), index => {
+      const line = start.line + index;
 
-      if (text.trim().length > 0) editor.insert(new vscode.Position(line, 0), tabSize);
+      const { range } = document.lineAt(line);
+
+      if (!range.isEmpty) editor.insert(line, 0, tabSize);
     });
 
-    startTagPosition = new vscode.Position(
-      start.line + 1,
-      Math.max(space.length, start.character) + 1 + Configuration.tag.length,
-    );
-    endTagPosition = new vscode.Position(end.line + 3, space.length + 2 + Configuration.tag.length);
+    return [ start.translate(1, 0), end.translate(3, 0) ];
+  } else {
+    editor.insert(start, `<${Configuration.tag}>\n`);
+    editor.insert(end, `\n</${Configuration.tag}>`);
+
+    times(Math.abs(start.line - end.line), index => {
+      const line = start.line + index + 1;
+
+      const { range } = document.lineAt(line);
+
+      if (!range.isEmpty) editor.insert(line, 0, tabSize);
+    });
+
+    return [ start.translate(0, 0), end.translate(2, 0) ];
   }
+};
+
+const tagWrapHandler: (textEditor: vscode.TextEditor) => TagWrapHandler = ({ selection, document }) => {
+  const { start, isEmpty, isSingleLine } = selection;
+  const { text, range } = document.lineAt(start.line);
+
+  if (isEmpty)
+    return start.character === range.end.character && text.trim().length > 0
+      ? isEmptyAndOnLastCharacter
+      : isEmptyAndNotOnLastCharacterAndEmpty;
+  else if (isSingleLine) return isSingleLineAndNotEmpty;
+  else return otherwiseHandler;
+};
+
+export default async function tagsWrap (textEditor: vscode.TextEditor) {
+  const { document, selections, options } = textEditor;
+
+  if (selections.length > 1) return;
+
+  const { length } = Configuration.tag;
+  const editor = new Editor(document);
+  const tabSize = options.insertSpaces ? ' '.repeat(<number>options.tabSize) : '\t';
+  const [ startTagPosition, endTagPosition ] = tagWrapHandler(textEditor)(textEditor, editor, tabSize);
+  const startSelection = new vscode.Selection(startTagPosition.translate(0, -length), startTagPosition);
+  const endSelection = new vscode.Selection(endTagPosition.translate(0, -length), endTagPosition);
 
   await editor.done();
 
-  if (!vscode.window.activeTextEditor) return;
-
-  vscode.window.activeTextEditor.selections = [
-    new vscode.Selection(startTagPosition.translate(0, -Configuration.tag.length), startTagPosition),
-    new vscode.Selection(endTagPosition.translate(0, -Configuration.tag.length), endTagPosition),
-  ];
+  textEditor.selections = [ startSelection, endSelection ];
 
   const dispose = () => {
     changeTextDocument.dispose();
@@ -81,20 +120,21 @@ export default async function tagsWrap ({ document, selections, selection, optio
   };
 
   const changeTextDocument = vscode.workspace.onDidChangeTextDocument(async ({ contentChanges, reason }) => {
-    const { activeTextEditor } = vscode.window;
+    const { selections, document } = textEditor;
 
-    if (!activeTextEditor || activeTextEditor.selections.length !== 2 || reason) return dispose();
+    if (selections.length !== 2 || reason) return dispose();
 
     const { text = '' } = contentChanges?.[0] ?? {};
 
     if ([ ' ', '\t' ].includes(text)) {
       await vscode.commands.executeCommand('undo');
 
-      const [ { end } ] = activeTextEditor.selections;
+      const [ { end } ] = selections;
+      const finalSelection = end.translate(0, 1);
 
-      await new Editor(activeTextEditor.document).insert(end, ' ').done();
+      await new Editor(document).insert(end, ' ').done();
 
-      activeTextEditor.selection = new vscode.Selection(end.translate(0, 1), end.translate(0, 1));
+      textEditor.selection = new vscode.Selection(finalSelection, finalSelection);
 
       dispose();
     }
